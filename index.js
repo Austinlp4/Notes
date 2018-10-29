0a83df17fcecf3b14bba55168086e2abbe0327bf
Austinlp4 <apendergrast44@gmail.com>
2018-10-29 11:05:41 -0500

    server up, sanity check working

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+//sanity check
+server.get('/', (req,res) => {
+    res.send('Everything is Working');
+});
+
+
+server.listen(5000, () => console.log('running on port 5000'));
